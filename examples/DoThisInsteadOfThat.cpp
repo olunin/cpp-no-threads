27d698a3b725c7b39e68333798fc4c794c103c72
Oleksandr Lunin <oleksandr.lunin@lighting.com>
2018-05-31 17:44:05 +0200

    enabled sanitizer

--- examples/DoThisInsteadOfThat.cpp
+++ examples/DoThisInsteadOfThat.cpp
@@ -1,11 +1,28 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <thread>
+
 class TestMock {
 public:
     TestMock() = default;
     MOCK_CONST_METHOD0(foo, void());
 };
+
+TEST(TheExample1, SanitizerCheck) {
+    int aa = 0;
+
+    std::thread t1([&aa]{
+        aa += 2;
+    });
+
+    std::thread t2([&aa]{
+        aa -= 3;
+    });
+
+    t1.join();
+    t2.join();
+}
 
 TEST(TheExample1, ExampleTest) {
     testing::StrictMock<TestMock> tt;
